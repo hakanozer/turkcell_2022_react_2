d55994a243378eb4d982dc695978dca55cab30f1
Hakan ÖZER <hakanozer02@gmail.com>
2022-10-05 15:50:19 +0300

    Product Detail

--- project/src/components/ProductDetail.tsx
+++ project/src/components/ProductDetail.tsx
@@ -8,10 +8,12 @@ function ProductDetail() {
   const navigate = useNavigate()
 
   const [item, setItem] = useState<ProBilgiler>({})
+  const [bigImage, setBigImage] = useState('')
   useEffect(() => {
     if ( location.state ) {
         const itm = location.state as ProBilgiler
         setItem(itm)
+        setBigImage( itm.images![0].normal )
       }else {
         navigate('/dashboard')
       }
@@ -21,8 +23,29 @@ function ProductDetail() {
   return (
     <>
         { item &&
+        <>
             <h2> { item.productName } </h2>
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <div className='mb-3'>
+                        <img src={ bigImage } className='img-fluid' />
+                    </div>
+                    
+                    <div>
+                        { item.images?.map( (item, index) =>
+                            <img onClick={()=> setBigImage(item.normal)  } role='button' key={index} src={ item.thumb } className='img-thumbnail m-1' />
+                        )}
+                    </div>
+                </div>
+                <div className='col-sm-6'>
+                    <h2><span className="badge bg-secondary">{ item.price } ₺</span></h2>
+                    <div className="content" dangerouslySetInnerHTML={{__html: item.description! }}></div>
+                    <button className='btn btn-success'><i className="bi bi-cart2"></i> Add Basket</button>
+                </div>
+            </div>    
+        </>
         }
+        
     </>
   )
 }

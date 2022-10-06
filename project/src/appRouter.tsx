8d49d366bd5d0589aee7a491c2355a9a2d74fc82
Hakan ÖZER <hakanozer02@gmail.com>
2022-10-06 11:57:24 +0300

    Redux - Setup

--- project/src/appRouter.tsx
+++ project/src/appRouter.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
@@ -7,16 +8,19 @@ import Login from './components/Login'
 import Product from './components/Product'
 import ProductDetail from './components/ProductDetail'
 import Security from './Security'
+import { store } from './useRedux/store'
 import { control } from './util'
 
 
 export const routes = 
-<BrowserRouter>
-    <ToastContainer/>
-    <Routes>
-        <Route path='' element={ control() === null ? <Login/> : <Navigate to={ localStorage.getItem('url') !== null ? localStorage.getItem('url')! : '/dashboard' } replace />} ></Route>
-        <Route path='/dashboard' element={  <Security component={ <Dashboard/> } />   } />
-        <Route path='/orders' element={  <Security component={ <Product/> } />   } />
-        <Route path='/productDetail' element={  <Security component={ <ProductDetail/> } />   } />
-    </Routes>
-</BrowserRouter>
+<Provider store={store}>
+    <BrowserRouter>
+        <ToastContainer/>
+        <Routes>
+            <Route path='' element={ control() === null ? <Login/> : <Navigate to={ localStorage.getItem('url') !== null ? localStorage.getItem('url')! : '/dashboard' } replace />} ></Route>
+            <Route path='/dashboard' element={  <Security component={ <Dashboard/> } />   } />
+            <Route path='/orders' element={  <Security component={ <Product/> } />   } />
+            <Route path='/productDetail' element={  <Security component={ <ProductDetail/> } />   } />
+        </Routes>
+    </BrowserRouter>
+</Provider>
---
+++ project/src/useRedux/store.tsx
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from "redux";
+import { OrderReducer } from './reducers/OrderReducer'
+
+const combine = combineReducers({
+    OrderReducer
+}) 
+
+export type StateType = ReturnType<typeof combine>
+
+export const store = createStore(combine) 
